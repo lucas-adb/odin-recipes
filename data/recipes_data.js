7cd358504b664bdac3fe69ca34ea598405918d93
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-07-26 09:43:30 -0300

    Creates recipes_data
    Make it easier to add more recipes in the future using .map

---
+++ data/recipes_data.js
@@ -0,0 +1,11 @@
+const recipesData = [
+  {
+    number: 131,
+    recipeName: "Apple Pie",
+    recipeImg: "https://www.zeldadungeon.net/wiki/images/6/6f/Apple_Pie_-_TotK_icon.png",
+    description: "The crispy, flaky pie crust and sweet apples are a match made in heaven.",
+    ingredientsName: ["Apple", "Tabantha Wheat", "Cane Sugar", "Goat Butter"],
+    ingredientsImgs: ["https://www.zeldadungeon.net/wiki/images/c/c3/Apple-botw.png?20190406170119", "https://www.zeldadungeon.net/wiki/images/5/58/Tabantha_Wheat.png?20170318003120", "https://www.zeldadungeon.net/wiki/images/6/67/Cane_Sugar.png?20170317192818", "https://www.zeldadungeon.net/wiki/images/6/68/Goat_Butter.png?20190327172603"],
+    effect: "Refill Heart Containers",
+  }
+]
